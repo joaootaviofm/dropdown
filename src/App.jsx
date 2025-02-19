514639d5689dfe26997cd21e6f66c68ce7d9020a
joaootaviofm <joaootaviofm@gmail.com>
2025-02-18 21:24:56 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Dropdown from "./components/Dropdown"
+
+function App() {
+
+  return (
+    <div className="grid place-items-center h-screen w-screen bg-gradient-to-r from-gray-100 to-teal-700">
+      <Dropdown />
+    </div>
+  )
+}
+
+export default App
